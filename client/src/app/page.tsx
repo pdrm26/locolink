6341e033f12a47e51ced3f0bf185231fd3d4e923
pedram <peeeedii@gmail.com>
2025-09-01 13:32:41 +0330

    feat: add sending message to another peer

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [peer, setPeer] = useState<Peer>();
+  const [message, setMessage] = useState<string>();
+  const [dstPeer, setDstPeer] = useState<string>();
 
   useEffect(() => {
     const peerID = Math.floor(Math.random() * 2 ** 18)
@@ -21,22 +23,46 @@ export default function Home() {
       console.log("My peer ID is ===> " + id);
     });
 
-    const conn = newPeer.connect("");
-
-    conn.on("open", function () {
-      console.log("+++++++++++++++++++++++++++++++hi");
-    });
-    conn.on("data", function (data) {
-      console.log("DATA::::", data);
-    });
-    conn.on("error", function (err) {
-      console.log("THE ERROR IS-------------------------", err);
-    });
-
     setPeer(newPeer);
 
     return () => newPeer.destroy();
   }, []);
 
-  return <div>client</div>;
+  function sendMessageToPeer() {
+    if (!peer || !dstPeer) return;
+    const conn = peer.connect(dstPeer);
+
+    conn.on("open", function () {
+      console.log("+++++++++++++++++++++++++++++++hi");
+      conn.send(message);
+    });
+    conn.on("data", function (data) {
+      console.log("DATA::::::::::", data);
+    });
+    conn.on("error", function (err) {
+      console.log("THE ERROR IS-------------------------", err);
+    });
+  }
+
+  return (
+    <>
+      <input
+        type="text"
+        name="message"
+        id="message"
+        placeholder="message"
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <br />
+      <input
+        type="text"
+        name="peerId"
+        id="peerId"
+        placeholder="peerId"
+        onChange={(e) => setDstPeer(e.target.value)}
+      />
+      <br />
+      <button onClick={sendMessageToPeer}>send</button>
+    </>
+  );
 }
