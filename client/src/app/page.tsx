692bcae32cb095c5db2acff9fee056313a6a3865
pedram <peeeedii@gmail.com>
2025-09-01 14:55:00 +0330

    feat: two peer can chat with each other

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,68 +1,102 @@
 "use client";
 
-import Peer from "peerjs";
-import { useEffect, useState } from "react";
+import Peer, { DataConnection } from "peerjs";
+import { useRef, useState } from "react";
 
 export default function Home() {
   const [peer, setPeer] = useState<Peer>();
-  const [message, setMessage] = useState<string>();
-  const [dstPeer, setDstPeer] = useState<string>();
+  const [connection, setConnection] = useState<DataConnection | undefined>();
+  const [messages, setMessages] = useState<string[]>([]);
 
-  useEffect(() => {
+  const peerRef = useRef(null);
+  const fpeerRef = useRef(null);
+  const msgRef = useRef(null);
+
+  function join() {
+    console.log("connecting...");
+
     const peerID = Math.floor(Math.random() * 2 ** 18)
       .toString(36)
       .padStart(4, "0");
-    const newPeer = new Peer(peerID, {
-      host: location.hostname,
+    const peer = new Peer(peerID, {
+      host: "localhost",
       port: 9000,
       path: "/",
-      debug: 3,
+      // debug: 3,
+    });
+    setPeer(peer);
+
+    // when connected is created, handle the event
+    peer.on("open", function (id) {
+      console.log("Connected to Signaling Server ID : " + id);
+      peerRef.current.value = id;
     });
 
-    newPeer.on("open", function (id) {
-      console.log("My peer ID is ===> " + id);
+    peer.on("connection", function (c) {
+      const conn = c;
+      console.log("New connection established");
+      // set the friend peer id we just got
+      fpeerRef.current.value = c.peer;
+      conn.on("open", function () {
+        // Receive messages - receiver side
+        conn.on("data", function (data) {
+          console.log("Received in connection: ", data);
+          setMessages((prev) => [...prev, data as string]);
+        });
+      });
     });
+  }
 
-    setPeer(newPeer);
+  function connect() {
+    const fpeerID = fpeerRef.current.value;
+    const conn = peer?.connect(fpeerID);
 
-    return () => newPeer.destroy();
-  }, []);
+    setConnection(conn);
 
-  function sendMessageToPeer() {
-    if (!peer || !dstPeer) return;
-    const conn = peer.connect(dstPeer);
+    // open event called when connection gets created
+    conn?.on("open", function () {
+      console.log("Connected");
 
-    conn.on("open", function () {
-      console.log("+++++++++++++++++++++++++++++++hi");
-      conn.send(message);
+      // Receive messages - sender side
+      conn.on("data", function (data) {
+        console.log("Received in connect: ", data);
+        setMessages((prev) => [...prev, data as string]);
+      });
     });
-    conn.on("data", function (data) {
-      console.log("DATA::::::::::", data);
-    });
-    conn.on("error", function (err) {
-      console.log("THE ERROR IS-------------------------", err);
-    });
+  }
+
+  function sendMessage() {
+    const msg = msgRef.current.value;
+    console.log("sending message: ", msg);
+
+    if (connection && connection.open) {
+      connection.send(msg);
+    }
   }
 
   return (
     <>
-      <input
-        type="text"
-        name="message"
-        id="message"
-        placeholder="message"
-        onChange={(e) => setMessage(e.target.value)}
-      />
+      <input id="peerid" placeholder="My ID" ref={peerRef} />
+      <button onClick={join}>Join</button>
       <br />
-      <input
-        type="text"
-        name="peerId"
-        id="peerId"
-        placeholder="peerId"
-        onChange={(e) => setDstPeer(e.target.value)}
-      />
+
+      <input type="text" id="fpeerid" placeholder="Peer ID" ref={fpeerRef} />
+      <button id="connect" onClick={connect}>
+        Connect
+      </button>
       <br />
-      <button onClick={sendMessageToPeer}>send</button>
+
+      <input type="text" id="msg" placeholder="Message.." ref={msgRef} />
+      <button id="send" onClick={sendMessage}>
+        Send
+      </button>
+      <br />
+
+      <ul id="messages">
+        {messages?.map((msg, index) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul>
     </>
   );
 }
