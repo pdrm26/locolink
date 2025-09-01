74def77a52f1ae7b8cba7d6ab4f08bca6034eb19
pedram <peeeedii@gmail.com>
2025-09-01 12:49:27 +0330

    feat: save the connected peers id and serve them with the /connected-people route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { ExpressPeerServer } from "peer";
 
+const connected = [];
+
 const app = express();
 app.get("/", (req, res) => res.send("Hello world!"));
 const port = 9000;
@@ -12,3 +14,17 @@ const peerServer = ExpressPeerServer(server, {
   allow_discovery: true,
 });
 app.use("/", peerServer);
+
+peerServer.on("connection", function (client) {
+  const clientID = client.getId();
+  if (connected.indexOf(clientID) === -1) connected.push(clientID);
+});
+
+peerServer.on("disconnect", function (client) {
+  const clientIdx = connected.indexOf(client.getId());
+  if (clientIdx !== -1) connected.splice(clientIdx, 1);
+});
+
+app.get("/connected-people", function (req, res) {
+  return res.json(connected);
+});
